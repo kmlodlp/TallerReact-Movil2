ddcf569de660c964c411f82220a8bee22e67cb5e
Camilo De La Peña <camilodelapena.estudiante@iser.edu.co>
2024-04-16 14:38:43 -0500

    Trabajo de movil portafolio

--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
+import DatosPerfil from '../data/PerfilData'
+import '../App.css'
 
 export const PerfilPage = () => {
     return (
         <>
-            <div className="container">
-                <div className="card mb-3" >
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="" alt="" />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+            <div className="Per-Container d-flex justify-content-center">
+                <div className="container mt-4 ">
+                    <div className="card mb-3" >
+                        <div className="row ">
+                            <div className="col-md-4">
+                                <img src="`${DatosPercil.img}`" alt="" />
                             </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">Nombres</h5>
+                                    <p className="card-text">{DatosPerfil.names}</p>
+                                    <h5 className="card-title">Apellidos</h5>
+                                    <p className="card-text">{DatosPerfil.Lastnames}</p>
+                                    <h5 className="card-title">Profesion</h5>
+                                    <p className="card-text"><small className="text-body-secondary">{DatosPerfil.Profession}</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="row m-3 justify-content-center">
+                        <div className="col-sm-9 d-flex justify-content-center">
+                            <p className="text-description">
+                                {DatosPerfil.description}
+                            </p>
                         </div>
                     </div>
                 </div>
